70ba3588bf67f732191ae36a5dc12b33f49741bb
Naseem Ali <naseem.ali.925@gmail.com>
2018-06-20 16:25:06 +0530

    Working On GameActivity

--- app/src/main/java/ali/naseem/keeper/GameActivity.java
+++ app/src/main/java/ali/naseem/keeper/GameActivity.java
@@ -1,13 +1,19 @@
 package ali.naseem.keeper;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private String RCB_PLAYERS=null,CSK_PLAYERS=null;
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +21,28 @@ public class GameActivity extends AppCompatActivity {
         Intent intent=getIntent();
         RCB_PLAYERS=intent.getStringExtra(Constants.PLAYERS_RCB);
         CSK_PLAYERS=intent.getStringExtra(Constants.PLAYERS_CSK);
+        drawer=(DrawerLayout)findViewById(R.id.drawer);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         Toast.makeText(this, RCB_PLAYERS, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.player1:
+                
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
